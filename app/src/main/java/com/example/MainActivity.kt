8166f17e8c058f76f6b3839f79a5ba827af25d3e
Babak <babakhanibabak@gmail.com>
2024-09-17 19:51:27 +0330

    execute classes

--- app/src/main/java/com/example/MainActivity.kt
+++ app/src/main/java/com/example/MainActivity.kt
@@ -1,9 +1,42 @@
 package com.example
 
+import com.example.innerClass.InnerClass
+import com.example.internal.VMInternalClass
 import com.example.sealedclass.Operation
+import com.example.nestedClass.NestedClass
+import com.example.visibilityModifiers.VMProtectedClass
 
 
 fun main(){
 val operationAdd=Operation.Add(5)
     operationAdd.addOperator()
+
+    //nestedClass
+    val userClass=NestedClass()
+    userClass.showName()
+    userClass.showName()
+    val userClassCity=NestedClass.City()
+    userClassCity.showCity()
+    val userClassAge=NestedClass.Age()
+    userClassAge.showAge()
+
+    //innerClass
+    val humanClass=InnerClass()
+    humanClass.showName()
+    humanClass.City().showCity()
+    humanClass.Age().showAge()
+      /*-------------------------------------------------------------------------------*/
+//protected Class
+val protectedClass=VMProtectedClass()
+
+
+    //internal Class
+    val internalClass= VMInternalClass()
+    internalClass.showName()
+    internalClass.name
+
+    /*از فراخوانی زیر چون در یک پکیج دیگر می باشد نمیتوان از آن استفاده کرد و این ویژگیinternalClass می باشد
+    internalClass.InternalChild().showName2()    */
+
+
 }
