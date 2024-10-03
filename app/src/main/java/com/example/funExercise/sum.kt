9a8954ce0db988b057a3ab67229f84cf61631aa6
Babak <babakhanibabak@gmail.com>
2024-10-03 20:19:57 +0330

    Add another sum function

--- app/src/main/java/com/example/funExercise/sum.kt
+++ app/src/main/java/com/example/funExercise/sum.kt
@@ -5,6 +5,14 @@ fun sumNumbers(number1:Int,number2:Int){
     println(a)
 }
 
+fun sumNumbers2(number1:Int,number2:Int):Int{
+    return number1+number2
+
+}
+
 fun main(){
     sumNumbers(5,5)
+
+    val a=sumNumbers2(5,5)+5
+    println(a)
 }
