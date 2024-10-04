c32d243ef74f1be5a8843f1d16e3483f94035392
Babak <babakhanibabak@gmail.com>
2024-10-04 21:21:46 +0330

    Add for loop in array

--- app/src/main/java/com/example/arrays/array.kt
+++ app/src/main/java/com/example/arrays/array.kt
@@ -13,5 +13,10 @@ fun main() {
         arrayOf(4, 5, 6),
         arrayOf(7, 8, 9)
     )
-
+    for (i in a3) {
+        for (j in i) {
+            println("$j")
+        }
+        println()
+    }
 }
