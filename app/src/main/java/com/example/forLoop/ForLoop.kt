ea0d71aa23764a84b07e56531e233f1efa89bfb0
Babak <babakhanibabak@gmail.com>
2024-10-07 19:30:52 +0330

    For loop examples

---
+++ app/src/main/java/com/example/forLoop/ForLoop.kt
@@ -0,0 +1,32 @@
+package com.example.forLoop
+
+fun main(){
+
+    for (i in 1..5){
+        for (j in 1..i){
+            print("*")
+        }
+        println()
+    }
+
+    // another example
+    for (x in 1..10){
+        println(x)
+    }
+    // another example
+    for (x in 1 until 10){
+        println(x)
+    }
+
+    // another example
+    for (x in 1 until 10 step 2){
+        println(x)
+    }
+
+    // another example
+    for (x in 10 downTo 1 step 2){
+        println(x)
+    }
+
+
+}
