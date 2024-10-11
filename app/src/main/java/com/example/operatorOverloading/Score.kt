6bcc77f11b852183c94fb9ad530dc3b788cb2e93
Babak <babakhanibabak@gmail.com>
2024-10-11 15:25:07 +0330

    Add another operator overLoading

--- app/src/main/java/com/example/operatorOverloading/Score.kt
+++ app/src/main/java/com/example/operatorOverloading/Score.kt
@@ -11,16 +11,33 @@ println("Plus is operator overLoading ${myScore +number}")
         println("Div is operator overLoading ${myScore /number}")
     }
 
+    infix operator fun times (number: Int){
+        println("times is operator overLoading ${myScore*number}")
+    }
+
+    infix operator fun minus(number: Int){
+        println("minus is operator overLoading ${myScore-number}")
+    }
 }
 
 
 fun main(){
     val score = Score()
 
-    //score.plus(5)
+    //plus operator
+   score.plus(5)
     score + 5
-    //score.div(4)
+
+    //divide operator
+    score.div(4)
     score / 4
 
+    //times operator
+    score.times(5)
+    score*3
+
+    //minus operator
+    score.minus(2)
+    score-3
 
 }
