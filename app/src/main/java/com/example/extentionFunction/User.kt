33eb60ce25ff5c4248da147d3ba98a65880643fc
Babak <babakhanibabak@gmail.com>
2024-10-14 19:39:02 +0330

    Execute Extension function

--- app/src/main/java/com/example/extentionFunction/User.kt
+++ app/src/main/java/com/example/extentionFunction/User.kt
@@ -1,6 +1,15 @@
 package com.example.extentionFunction
 
+import com.example.getValues
+import com.example.setValues
+
 class User {
 var name:String=""
 var family:String=""
+}
+
+fun main(){
+    val user=User()
+    user.setValues("Babak", "Babakhani")
+    user.getValues()
 }
