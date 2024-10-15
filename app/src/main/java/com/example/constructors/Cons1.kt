fddfc8ef6457a4dd9fab7aa1259e51231534bfa0
Babak <babakhanibabak@gmail.com>
2024-10-15 22:21:10 +0330

    Execute another constructor function

--- app/src/main/java/com/example/constructors/Cons1.kt
+++ app/src/main/java/com/example/constructors/Cons1.kt
@@ -1,24 +1,30 @@
 package com.example.constructors
 
 class Cons1(id:Int) {
-    val name="$id => Mohammad"
+
     init {
         when(id){
             1 -> {
-                println("$name Babak")
+                println(" Babak")
             }
             2 ->{
-                println("$name Foad")
+                println(" Foad")
             }
             3->{
-                println("$name hosain")
+                println(" hosain")
             }
         }
     }
     constructor(name: String,family:String,id: Int):this(id){
         println( "$name $family $id")
     }
-    constructor(instaId:String,id: Int):this(id){
-        println(instaId)
+    constructor(instaName:String, id: Int):this(id){
+        println(instaName)
     }
+}
+
+fun main(){
+    val user=Cons1("Babak" ,5)
+    val user2=Cons1(name = "Babak", family = "Babakhani",34)
+    val user3=Cons1(1)
 }
