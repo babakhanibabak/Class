8cbaf2492ba0550f45e14e054c4df0c2da1766b5
Babak <babakhanibabak@gmail.com>
2024-10-17 21:46:49 +0330

    Execute 2 another class

--- app/src/main/java/com/example/sealedclass/Operation.kt
+++ app/src/main/java/com/example/sealedclass/Operation.kt
@@ -1,12 +1,14 @@
 package com.example.sealedclass
 
+import android.graphics.Path.Op
+
 
 sealed class Operation {
     val number = 2
 
     class Add(private val value: Int) : Operation() {
         fun addOperator() {
-            print(value + number)
+            println(value + number)
         }
     }
 
@@ -35,7 +37,14 @@ fun main() {
 
     val operation = Operation.Add(5)
     operation.addOperator()
+
     val operation2 = Operation.Multiply(3)
     operation2.multiplyOperator()
+
+    val operation3=Operation.Subtract(10)
+    operation3.subtractOperator()
+
+    val operation4=Operation.Divide(12)
+    operation4.divideOperator()
 
 }
