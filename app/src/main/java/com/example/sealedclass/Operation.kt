da72e45632f8f64cfd7a3a0d1752baa79ad8c401
Babak <babakhanibabak@gmail.com>
2024-10-17 21:44:13 +0330

    Add 2 another class

--- app/src/main/java/com/example/sealedclass/Operation.kt
+++ app/src/main/java/com/example/sealedclass/Operation.kt
@@ -1,27 +1,41 @@
 package com.example.sealedclass
 
-import android.graphics.Path.Op
 
 sealed class Operation {
-    val number=2
-    class Add(private val value: Int):Operation(){
-        fun addOperator(){
-            print(value+number)
+    val number = 2
+
+    class Add(private val value: Int) : Operation() {
+        fun addOperator() {
+            print(value + number)
         }
     }
 
-    class Multiply(private val value:Int):Operation(){
-        fun multiplyOperator(){
-            println(value*number)
+    class Multiply(private val value: Int) : Operation() {
+        fun multiplyOperator() {
+            println(value * number)
         }
     }
 
+    class Subtract(private val value: Int) : Operation() {
+        fun subtractOperator() {
+            println(value - number)
+        }
+    }
+
+    class Divide(private val value: Int) : Operation() {
+        fun divideOperator() {
+            println(value / number)
+        }
+    }
 
 
 }
 
-fun main(){
+fun main() {
 
-    val operation=Operation.Add(5)
-operation.addOperator()
+    val operation = Operation.Add(5)
+    operation.addOperator()
+    val operation2 = Operation.Multiply(3)
+    operation2.multiplyOperator()
+
 }
