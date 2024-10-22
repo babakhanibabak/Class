76f537a582839dbd7a9571510fdcbef4fba2e019
Babak <babakhanibabak@gmail.com>
2024-10-22 16:57:30 +0330

    Add another example inside Data class

--- app/src/main/java/com/example/dataClass/Student.kt
+++ app/src/main/java/com/example/dataClass/Student.kt
@@ -7,7 +7,14 @@ data class Student(
 )
 
 fun main(){
+
     val student=Student("Babak",34)
     println(student)
     println(student.age)
+
+
+    // another way for use of copy
+    val student2=student.copy("foad",41)
+    println(student2)
+    println(student2.name)
 }
