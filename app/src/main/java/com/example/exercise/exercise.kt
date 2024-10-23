2166587b80ec9b8b4c9c48941d7f3249e3337a7f
Babak <babakhanibabak@gmail.com>
2024-10-23 11:19:10 +0330

    Do Exercise of kotlin

---
+++ app/src/main/java/com/example/exercise/exercise.kt
@@ -0,0 +1,27 @@
+package com.example.exercise
+
+fun main(){
+
+    //length of string
+    val text="BabakBabakhani"
+    println(text.length) // output is 14
+
+    //compare 2 string
+    val txt1="apple"
+    val txt2="apple"
+    println(txt1.compareTo(txt2)) //output is 0 because 2 string is equal
+
+    //find string inside string
+    val txt = "Please locate where 'locate' occurs!"
+    println(txt.indexOf("locate")) //output is 7
+
+    //merge 2 strings
+    var firstname="Babak"
+    var lastname="Babakhani"
+    println(firstname+" "+lastname)
+    //merge 2 strings
+    println(firstname.plus(lastname))
+
+
+
+}
