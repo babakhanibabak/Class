3a75519add60da0aa4c106475c39d6ba55a61e11
Babak <babakhanibabak@gmail.com>
2024-10-24 16:01:46 +0330

    Do Exercise for break

---
+++ app/src/main/java/com/example/exercise/breakExercise.kt
@@ -0,0 +1,15 @@
+package com.example.exercise
+
+fun main(){
+
+    //break
+
+    var x=1
+    while (x<10){
+        println(x)
+        x++
+        if (x==4){
+            break
+        }
+    }
+}
