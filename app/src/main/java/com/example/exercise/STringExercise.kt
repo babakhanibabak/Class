bcf7b28dabb0004e4ea0f3399a3f46fe88429036
Babak <babakhanibabak@gmail.com>
2024-10-26 20:59:54 +0330

    Do Exercise for Strings

--- app/src/main/java/com/example/exercise/STringExercise.kt
+++ app/src/main/java/com/example/exercise/STringExercise.kt
@@ -1,14 +1,14 @@
 package com.example.exercise
 
-fun main(){
+fun main() {
 
     //length of string
-    val text="BabakBabakhani"
+    val text = "BabakBabakhani"
     println(text.length) // output is 14
 
     //compare 2 string
-    val txt1="apple"
-    val txt2="apple"
+    val txt1 = "apple"
+    val txt2 = "apple"
     println(txt1.compareTo(txt2)) //output is 0 because 2 string is equal
 
     //find string inside string
@@ -16,9 +16,9 @@ fun main(){
     println(txt.indexOf("locate")) //output is 7
 
     //merge 2 strings
-    var firstname="Babak"
-    var lastname="Babakhani"
-    println(firstname+" "+lastname)
+    var firstname = "Babak"
+    var lastname = "Babakhani"
+    println(firstname + " " + lastname)
     //merge 2 strings
     println(firstname.plus(lastname))
 
